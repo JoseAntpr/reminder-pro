4943ab20af9e62c95fd9971fe7c5f2fd2229f998
José Antonio <joseantpalaciosramirez@gmail.com>
2017-12-15 08:18:02 +0100

    Mapping Dispatch To Props and Binding Action Creators
    Learn how to connect actions with props in the jsx file.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions/index';
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    addReminder(){
+        console.log('this', this);
+        this.props.addReminder(this.state.text);
+    }
     render(){
         return(
             <div className="app">
@@ -12,11 +26,13 @@ class App extends Component {
                         <input 
                             className="form-control"
                             placeholder="I have to ..."
+                            onChange={event => this.setState({text: event.target.value})}
                         />
                     </div>
                     <button
                         type="button"
                         className="btn btn-success"
+                        onClick={() => this.addReminder()}
                     >
                     Add reminder
                     </button>
@@ -26,4 +42,6 @@ class App extends Component {
     }
 }
 
-export default App
+
+
+export default connect(null, {addReminder})(App);
